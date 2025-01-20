c5e5352da81fc89bfe128607ec899c3937560aa3
Dmitry Skachkov <d.g.skachkov@gmail.com>
2025-01-20 13:16:48 -0600

    Update main.F90

--- Example/main.F90
+++ Example/main.F90
@@ -2,6 +2,10 @@
 
     program main
      use one
+     real(8)    :: t0,tf
+     call cpu_time(t0)
      call calc_matr
+     call cpu_time(tf)
+     print *,'calc time =',tf-t0,' s'
     end program main
 
